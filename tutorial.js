be264dce5d05fe4bea3d811aa355b3a2bd366fcb
Jason <Jason@Jasons-MacBook-Air.local>
2015-01-05 16:33:09 -0500

    first commit for this group project

---
+++ tutorial.js
@@ -0,0 +1,64 @@
+// SCROLLTOTOP BUTTON
+// $(document).ready(function(){
+    
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 400) {
+            $('#backTop').fadeIn();
+        } else {
+            $('#backTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('#backTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+    $('#installMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#installPage').offset().top
+    	}, 800);
+    });
+
+    $('#clientMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#clientPage').offset().top
+    	}, 800);
+    });
+    
+    $('#serverMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#serverPage').offset().top
+    	}, 800);
+    });
+
+    $('#styleMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#stylePage').offset().top
+    	}, 800);
+    });
+
+    $('#gitMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#gitPage').offset().top
+    	}, 800);
+    });
+
+    $('#hostMove').click(function(){
+    	$('html, body').animate({
+    		scrollTop: $('#hostPage').offset().top
+    	}, 800);
+    });
+
+// });
+
+
+
+
+
+
+
+
+
